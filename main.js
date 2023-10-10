557a00e36de047c440777c6dede3570645f6ab44
gksucalvi <106171918+gksucalvi@users.noreply.github.com>
2023-10-10 08:27:34 -0300

    10/09

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ function tocaSomPom() {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+while(contador < 9){
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log;
+}
